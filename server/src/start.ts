e27ebe7286894192bd7ebbf808bc001390448de7
emilsvennesson <emilsvennesson@users.noreply.github.com>
2023-02-07 13:33:49 +0100

    setup react & tailwind

--- server/src/start.ts
+++ server/src/start.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from "cors";
 import { ticketRouter } from "./router/TicketRouter";
 
 export const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/ticket", ticketRouter);

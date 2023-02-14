676e82374e8ace54a8e9a9154fd807ccb4bc4638
Pouya <sokhan@student.chalmers.se>
2023-02-14 11:30:34 +0100

    update ticket system

--- client/src/pages/admin/tickets/AdminTicketItem.tsx
+++ client/src/pages/admin/tickets/AdminTicketItem.tsx
@@ -1,20 +1,24 @@
 import { Badge } from 'flowbite-react';
+import { Link } from 'react-router-dom';
 import Ticket from '../../../model/Ticket';
 
 export default function AdminTicketItem({ id, title, open }: Ticket) {
   return (
-    <button
+    <Link
+      to={`/admin/tickets/${id}`}
       className="grid grid-flow-col grid-cols-10 items-center gap-x-1 py-2 hover:bg-slate-300"
       type="button"
     >
-      <p className="col-span-3 h-6 truncate text-left">{id}</p>
-      <p className="col-span-5 h-6 truncate text-left">{title}</p>
+      <p className="col-span-3 h-6 truncate text-left md:col-span-2">{id}</p>
+      <p className="col-span-1 hidden md:flex">User</p>
+      <p className="col-span-5 h-6 truncate text-left sm:col-span-4">{title}</p>
       <Badge
         color={open ? 'success' : 'failure'}
         className="col-span-2 mx-5 h-6 w-fit justify-self-center text-center"
       >
         {open ? 'Open' : 'Closed'}
       </Badge>
-    </button>
+      <p className="col-span-1 hidden sm:flex">Date</p>
+    </Link>
   );
 }
--- client/src/pages/admin/tickets/AdminTicketsPage.tsx
+++ client/src/pages/admin/tickets/AdminTicketsPage.tsx
@@ -27,16 +27,18 @@ export default function TicketListPage() {
 
   return (
     <div className="flex h-max justify-center">
-      <div className="bg bg-color=5 bg-blue flex w-full flex-col items-center gap-2 p-5 lg:w-3/4 xl:w-2/3">
+      <div className="bg bg-color=5 bg-blue flex w-full flex-col items-center gap-2 p-5">
         <Card className="h-fit w-full">
           <h1 className="text-3xl font-bold text-gray-800">Tickets</h1>
         </Card>
         <Card className="w-full">
-          <div className="-mb-2 flex w-full flex-col border-b-2 border-slate-500">
+          <div className="-mb-4 flex w-full flex-col border-b-2 border-slate-500">
             <div className="grid grid-flow-col grid-cols-10 gap-x-1 text-lg font-semibold">
-              <p className="col-span-3 ">ID</p>
-              <p className="col-span-5">Title</p>
+              <p className="col-span-3 md:col-span-2">ID</p>
+              <p className="col-span-1 hidden md:flex">User</p>
+              <p className="col-span-5 sm:col-span-4">Title</p>
               <p className="col-span-2 text-center">Status</p>
+              <p className="col-span-1 hidden sm:flex">Date</p>
             </div>
           </div>
           <div className="flex w-full flex-col divide-y-2 divide-slate-300">

61b8240b4e8cba82f942dbf08510f0b0224ec724
Albin Sundström <albinsundstrom@hotmail.com>
2023-02-14 12:12:46 +0100

    change form from raw tailwind to flowbite

--- client/src/pages/client/TicketFormPage.tsx
+++ client/src/pages/client/TicketFormPage.tsx
@@ -1,3 +1,4 @@
+import { Button, Label, Textarea, TextInput } from 'flowbite-react';
 import { useState } from 'react';
 
 export default function TicketFormPage() {
@@ -39,48 +40,38 @@ export default function TicketFormPage() {
   return (
     <div className="mx-auto h-screen">
       <div className="flex h-max justify-center">
-        <form className="bg bg-color=5 bg-blue flex w-full flex-col items-center gap-2 bg-slate-200 p-5 lg:w-3/4 xl:w-2/3">
+        <form className=" flex w-full flex-col gap-4 p-4 lg:w-3/4 xl:w-2/3">
           <div>
-            <p className="text-3xl font-bold text-gray-800">Create ticket</p>
+            <div className="mb-2 block">
+              <Label htmlFor="title" value="Title" />
+            </div>
+            <TextInput
+              id="title"
+              type="text"
+              placeholder="Title"
+              onChange={handleTitleChange}
+            />
           </div>
-          <div className="w-full border-2">
-            <label
-              className="mb-2 block text-xl font-bold text-gray-700"
-              htmlFor="title"
-            >
-              Title:
-              <input
-                className="focus:shadow-outline w-full appearance-none rounded border py-2 px-3 font-normal leading-tight text-gray-700 shadow focus:outline-none"
-                id="title"
-                type="text"
-                placeholder="Title"
-                onChange={handleTitleChange}
-              />
-            </label>
-          </div>
-          <div className="w-full border-2">
-            <label
-              className="mb-2 block text-xl font-bold text-gray-700"
-              htmlFor="description"
-            >
-              Description:
-              <textarea
-                className="focus:shadow-outline w-full resize-none appearance-none rounded border py-2 px-3 font-normal leading-tight text-gray-700 shadow focus:outline-none"
-                id="description"
-                placeholder="Description"
-                rows={5}
-                onChange={handleDescriptionChange}
-              />
-            </label>
+          <div>
+            <div className="mb-2 block">
+              <Label value="Description" />
+            </div>
+            <Textarea
+              className="focus:shadow-outline w-full resize-none appearance-none rounded border py-2 px-3 font-normal leading-tight text-gray-700 shadow focus:outline-none"
+              id="description"
+              placeholder="Description"
+              rows={5}
+              onChange={handleDescriptionChange}
+            />{' '}
           </div>
           <div className="flex w-full justify-end">
-            <button
-              className="w-32 rounded bg-orange-600 py-2 px-4 text-xl font-bold text-white hover:bg-orange-700"
-              type="button"
+            <Button
+              className="w-full bg-blue-500 lg:w-1/5"
+              type="submit"
               onClick={handleSubmitForm}
             >
               Submit
-            </button>
+            </Button>
           </div>
         </form>
       </div>

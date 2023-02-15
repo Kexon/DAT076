48f77d42fe562126af885ec10e01a5cf2aba6da5
emilsvennesson <emilsvennesson@users.noreply.github.com>
2023-02-15 10:38:23 +0100

    refactor API calls to a service

---
+++ client/src/services/ApiService.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import { Ticket, NewTicket } from '../model/Ticket';
+
+const API_URL = 'http://localhost:8080';
+
+export default class ApiService {
+  static async getTickets(): Promise<Ticket[]> {
+    const { data } = await axios.get<Ticket[]>(`${API_URL}/ticket`);
+    return data;
+  }
+
+  static async getTicket(id: number): Promise<Ticket> {
+    const { data } = await axios.get<Ticket>(`${API_URL}/ticket/${id}`);
+    return data;
+  }
+
+  static async createTicket(ticket: NewTicket): Promise<Ticket> {
+    const { data } = await axios.post<Ticket>(`${API_URL}/ticket`, ticket);
+    return data;
+  }
+}

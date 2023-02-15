f11d35d65169690eee862bea124dd4381251e7fc
emilsvennesson <emilsvennesson@users.noreply.github.com>
2023-02-15 13:23:52 +0100

    add button to open/close ticket

--- client/src/pages/admin/tickets/TicketUserItem.tsx
+++ client/src/pages/admin/tickets/TicketUserItem.tsx
@@ -1,5 +1,7 @@
-import { Avatar } from 'flowbite-react';
+import { Avatar, Button } from 'flowbite-react';
+import { useState } from 'react';
 import { Ticket } from '../../../model/Ticket';
+import ApiService from '../../../services/ApiService';
 
 interface Props {
   ticket?: Ticket;
@@ -7,10 +9,18 @@ interface Props {
 
 export default function TicketUserItem({ ticket }: Props) {
   if (!ticket) return <div>Loading...</div>;
+
+  const [isTicketOpen, setIsTicketOpen] = useState<boolean>(ticket.open);
+  const onTicketButtonClick = () => {
+    setIsTicketOpen((ticketStatus) => !ticketStatus);
+    if (isTicketOpen) ApiService.closeTicket(ticket.id);
+    else ApiService.openTicket(ticket.id);
+  };
+
   return (
-    <div className="col-span-2 mt-1 max-h-[250px] rounded-sm bg-white p-2 shadow-lg">
+    <div className="col-span-2 mx-3 mt-1 max-h-[250px] rounded-sm bg-white p-2 shadow-lg">
       <div className="flex justify-center">
-        <div className="lg:text-2x flex text-xl">
+        <div className="lg:text-2x flex text-base lg:text-lg">
           <h1 className="">Ticket&nbsp; </h1>
           <h1
             className="font-bold
@@ -25,12 +35,21 @@ export default function TicketUserItem({ ticket }: Props) {
         className="flex min-h-[200px] items-center justify-center 
   "
       >
-        <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center gap-1">
           <Avatar rounded size="lg" />
           <div className="flex">
             <h1 className="text-lg font-thin">Created by&nbsp; </h1>
             <h1 className="text-lg font-bold">John Doe</h1>
           </div>
+          {!isTicketOpen ? (
+            <Button color="success" size="xs" onClick={onTicketButtonClick}>
+              Open ticket
+            </Button>
+          ) : (
+            <Button color="failure" size="xs" onClick={onTicketButtonClick}>
+              Close ticket
+            </Button>
+          )}
         </div>
       </div>
     </div>

1e698a580f72ec2861a61ad22015196aa1e92c1f
Pouya Shirin <kexon@hotmail.se>
2023-02-16 18:35:06 +0100

    add comment

--- client/src/__tests__/AdminTicketItem.tsx
+++ client/src/__tests__/AdminTicketItem.tsx
@@ -5,6 +5,7 @@ import MockTicket from '../utils/MockTicket';
 
 test('Ticket should show up on screen', () => {
   render(<AdminTicketItem ticket={MockTicket} />, { wrapper: BrowserRouter });
+  // ^name$ will check for exact match
   const titleElement = screen.getByText(/^Test Ticket$/);
   expect(titleElement).toBeInTheDocument();
 });

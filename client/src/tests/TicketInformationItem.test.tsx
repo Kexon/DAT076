20210c1093bb8e30305b023d078bd9651748e380
emilsvennesson <emilsvennesson@users.noreply.github.com>
2023-02-16 14:26:11 +0100

    create test for view ticket components

---
+++ client/src/tests/TicketInformationItem.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import TicketInformationItem from '../pages/admin/tickets/TicketInformationItem';
+import MockTicket from './MockTicket';
+
+test('the title and description should be displayed', () => {
+  render(<TicketInformationItem ticket={MockTicket} />, {
+    wrapper: BrowserRouter,
+  });
+  const titleElement = screen.getByText(/^Test Ticket$/);
+  const descElement = screen.getByText(/^This is a test ticket$/);
+  expect(titleElement).toBeInTheDocument();
+  expect(descElement).toBeInTheDocument();
+});
---
+++ client/src/tests/TicketUserItem.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import TicketUserItem from '../pages/admin/tickets/TicketUserItem';
+import MockTicket from './MockTicket';
+
+test('the id should be displayed', () => {
+  render(<TicketUserItem ticket={MockTicket} />, { wrapper: BrowserRouter });
+  const idElement = screen.getByText(/^#1$/);
+  expect(idElement).toBeInTheDocument();
+});
+
+test('component should be displaying loading when ticket is undefined', () => {
+  render(<TicketUserItem />, { wrapper: BrowserRouter });
+  const idElement = screen.getByText(/^Loading...$/);
+  expect(idElement).toBeInTheDocument();
+});

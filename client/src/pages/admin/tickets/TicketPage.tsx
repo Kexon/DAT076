c62faf16689128875b0b2ea8aef0de7729bd6818
emilsvennesson <emilsvennesson@users.noreply.github.com>
2023-02-16 14:26:49 +0100

    return "ticket not found" in tickets page on wrong id

--- client/src/pages/admin/tickets/TicketPage.tsx
+++ client/src/pages/admin/tickets/TicketPage.tsx
@@ -9,16 +9,22 @@ import TicketUserItem from './TicketUserItem';
 
 export default function TicketPage() {
   const [ticket, setTicket] = useState<Ticket>();
+  const [loading, setLoading] = useState(true);
   const { id } = useParams();
+
   useEffect(() => {
     const getTicket = async () => {
       if (id) {
         const data = await ApiService.getTicket(parseInt(id, 10));
         setTicket(data);
+        setLoading(false);
       }
     };
     getTicket();
   }, [id]);
+
+  if (!loading && !ticket) return <div>Ticket not found.</div>;
+
   return (
     <div className="grid-cols-6 gap-4 md:grid">
       <TicketInformationItem ticket={ticket} />

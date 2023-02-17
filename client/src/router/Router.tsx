2a09f6f68bdd48fa23b727d7d805374f9cfa4ddb
Pouya Shirin <kexon@hotmail.se>
2023-02-17 15:58:48 +0100

    add router path to UserPage

--- client/src/router/Router.tsx
+++ client/src/router/Router.tsx
@@ -1,9 +1,10 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Navigate } from 'react-router-dom';
 import AdminTicketsPage from '../pages/admin/tickets/AdminTicketsPage';
 import TicketPage from '../pages/admin/tickets/TicketPage';
 import TicketFormPage from '../pages/user/TicketFormPage';
 import Home from '../pages/Home';
 import Root from '../Root';
+import UserPage from '../pages/user/UserPage';
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,9 @@ const router = createBrowserRouter([
       { path: 'createticket', element: <TicketFormPage /> },
       { path: 'admin', element: <AdminTicketsPage /> },
       { path: 'ticket/:id', element: <TicketPage /> },
+      // Maybe dashboard should be a child element of user?
+      { path: 'user', element: <Navigate to="user/dashboard" /> }, // Not sure if a good way to redirect
+      { path: 'user/dashboard', element: <UserPage /> },
     ],
   },
 ]);

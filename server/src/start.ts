59e3213e3c523d2fa1fa382cd6400d5e2233cdc6
emilsvennesson <emilsvennesson@users.noreply.github.com>
2023-02-21 16:26:45 +0100

    setup express-session

--- server/src/start.ts
+++ server/src/start.ts
@@ -1,9 +1,25 @@
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import { ticketRouter } from "./router/TicketRouter";
+import { userRouter } from "./router/UserRouter";
+import session from "express-session";
 
 export const app = express();
 
+app.use(
+  session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 app.use(express.json());
-app.use(cors());
 app.use("/ticket", ticketRouter);
+app.use("/user", userRouter);

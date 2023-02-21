6e303fe1e45799dc8af861f4626b7068bac9f87b
emilsvennesson <emilsvennesson@users.noreply.github.com>
2023-02-21 16:56:26 +0100

    more type fixing
    Co-Authored-By: Pouya Shirin <13919156+Kexon@users.noreply.github.com>
    Co-Authored-By: Albin Sundström <73998992+sundstromalbin@users.noreply.github.com>
    Co-Authored-By: Pouya Shirin <13919156+Kexon@users.noreply.github.com>
    Co-Authored-By: Albin Sundström <73998992+sundstromalbin@users.noreply.github.com>

--- server/src/router/UserRouter.ts
+++ server/src/router/UserRouter.ts
@@ -2,33 +2,13 @@ import express, { Request, Response } from "express";
 import makeUserService from "../service/user/UserService";
 import { UserInfo } from "../model/User";
 
-type UserSession = Request & {
-  session: {
-    user?: UserInfo;
-  };
-};
-
-interface UserRequest extends UserSession {
-  body: {
-    username: string;
-    password: string;
-  };
+interface UserRequest {
+  username: string;
+  password: string;
 }
+
 const userService = makeUserService();
 export const userRouter = express.Router();
-
-userRouter.get(
-  "/",
-  async (req: UserSession, res: Response<UserInfo | string>) => {
-    const user = await userService.getUser(req.session.user?.id || "");
-    if (!user) {
-      res.status(401).send(`Failed to get user`);
-      return;
-    }
-    res.status(201).send(user);
-    return;
-  }
-);
 
 userRouter.get(
   "/:id",
@@ -36,9 +16,13 @@ userRouter.get(
     req: Request<{ id: string }, {}, {}>,
     res: Response<UserInfo | string>
   ) => {
-    const user = await userService.getUser(req.session.user?.id || "");
+    if (!req.session.user) {
+      res.status(401).send("You are not logged in");
+      return;
+    }
+    const user = await userService.getUser(req.params.id);
     if (!user) {
-      res.status(401).send(`Failed to get user`);
+      res.status(401).send("User does not exist");
       return;
     }
     res.status(201).send(user);
@@ -46,9 +30,22 @@ userRouter.get(
   }
 );
 
+userRouter.get("/", async (req, res: Response<UserInfo | string>) => {
+  const user = await userService.getUser(req.session.user?.id || "");
+  if (!user) {
+    res.status(401).send(`Failed to get user`);
+    return;
+  }
+  res.status(201).send(user);
+  return;
+});
+
 userRouter.post(
   "/",
-  async (req: UserRequest, res: Response<UserInfo | string>) => {
+  async (
+    req: Request<{}, {}, UserRequest>,
+    res: Response<UserInfo | string>
+  ) => {
     if (typeof req.body.username !== "string") {
       res
         .status(400)
@@ -87,38 +84,33 @@ userRouter.post(
   }
 );
 
-userRouter.post(
-  "/login",
-  async (req: UserRequest, res: Response<UserInfo | string>) => {
-    if (typeof req.body.username !== "string") {
-      res
-        .status(400)
-        .send(
-          `Bad POST call to ${
-            req.originalUrl
-          } --- username has type ${typeof req.body.username}`
-        );
-      return;
-    }
-
-    if (typeof req.body.password !== "string") {
-      res
-        .status(400)
-        .send(
-          `Bad POST call to ${
-            req.originalUrl
-          } --- password has type ${typeof req.body.password}`
-        );
-      return;
-    }
-    const user = await userService.login(req.body.username, req.body.password);
-    if (user) {
-      req.session.user = user;
-      res.status(200).send(user);
-      return;
-    } else {
-      res.status(401).send("Wrong username or password");
-      return;
-    }
+userRouter.post("/login", async (req, res: Response<UserInfo | string>) => {
+  if (typeof req.body.username !== "string") {
+    res
+      .status(400)
+      .send(
+        `Bad POST call to ${req.originalUrl} --- username has type ${typeof req
+          .body.username}`
+      );
+    return;
   }
-);
+
+  if (typeof req.body.password !== "string") {
+    res
+      .status(400)
+      .send(
+        `Bad POST call to ${req.originalUrl} --- password has type ${typeof req
+          .body.password}`
+      );
+    return;
+  }
+  const user = await userService.login(req.body.username, req.body.password);
+  if (user) {
+    req.session.user = user;
+    res.status(200).send(user);
+    return;
+  } else {
+    res.status(401).send("Wrong username or password");
+    return;
+  }
+});

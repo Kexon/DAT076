3b4606b9b1db0399ad1a4c0398ceacf765d25b6e
Pouya Shirin <kexon@hotmail.se>
2023-02-21 10:00:14 +0100

    refactor user page structure

--- client/src/pages/user/UserPage.tsx
+++ client/src/pages/user/UserPage.tsx
@@ -1,33 +1,13 @@
 import { Card } from 'flowbite-react';
-import { useEffect, useState } from 'react';
-import { Ticket } from '../../model/Ticket';
-import ApiService from '../../services/ApiService';
-import AdminTicketItem from '../admin/tickets/AdminTicketItem';
-import UserPageTicket from './UserPageTicketItem';
+import UserPageTickets from './UserTickets';
 
 export default function UserPage() {
-  const [tickets, setTickets] = useState<Ticket[]>([]);
-  useEffect(() => {
-    const getTickets = async () => {
-      const data = await ApiService.getTickets();
-      setTickets(data);
-    };
-    getTickets();
-  }, []);
-
   return (
     <div>
       <Card className="text-3xl font-semibold">Greetings username</Card>
       <div className="flex flex-col sm:flex-row">
         <Card className="sm:flex-1">Account Settings</Card>
-        <Card className="divide-slate-400 sm:flex-1">
-          <h2 className="text-lg font-medium">My tickets</h2>
-          <div className="flex flex-col flex-wrap">
-            {tickets.map((ticket) => (
-              <UserPageTicket ticket={ticket} />
-            ))}
-          </div>
-        </Card>
+        <UserPageTickets />
       </div>
     </div>
   );
---
+++ client/src/pages/user/UserTickets.tsx
@@ -0,0 +1,26 @@
+import { Card } from 'flowbite-react';
+import { useState, useEffect } from 'react';
+import { Ticket } from '../../model/Ticket';
+import ApiService from '../../services/ApiService';
+import UserPageTicket from './UserTicketItem';
+
+export default function UserPage() {
+  const [tickets, setTickets] = useState<Ticket[]>([]);
+  useEffect(() => {
+    const getTickets = async () => {
+      const data = await ApiService.getTickets();
+      setTickets(data);
+    };
+    getTickets();
+  }, []);
+  return (
+    <Card className="divide-slate-400 sm:flex-1">
+      <h2 className="text-lg font-medium">My tickets</h2>
+      <div className="flex flex-col flex-wrap">
+        {tickets.map((ticket) => (
+          <UserPageTicket ticket={ticket} />
+        ))}
+      </div>
+    </Card>
+  );
+}

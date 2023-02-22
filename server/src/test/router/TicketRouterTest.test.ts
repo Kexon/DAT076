f5afb0ddc1d0baa883f741c173ada7badd7cbdbf
Pouya Shirin <kexon@hotmail.se>
2023-02-22 10:36:58 +0100

    fix tests accordingly

--- server/src/test/router/TicketRouterTest.test.ts
+++ server/src/test/router/TicketRouterTest.test.ts
@@ -4,13 +4,13 @@ import { app } from "../../start";
 const request = supertest(app);
 
 test("Test that the tickets endpoint exists and return status code 200", async () => {
-  const res = await request.get("/tickets");
+  const res = await request.get("/ticket");
   expect(res.status).toEqual(200);
 });
 
 test("Test that a ticket gets posted with correct title and description", async () => {
   const res = await request
-    .post("/tickets")
+    .post("/ticket")
     .send({ title: "title", description: "desc" });
   expect(res.status).toEqual(201);
   expect(res.body.title).toEqual("title");

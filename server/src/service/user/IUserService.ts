38e5bfde76cf18f676d8e238ae19e133c2cf1297
Pouya Shirin <kexon@hotmail.se>
2023-02-22 10:39:22 +0100

    add comment

--- server/src/service/user/IUserService.ts
+++ server/src/service/user/IUserService.ts
@@ -1,7 +1,15 @@
 import { UserInfo } from "../../model/User";
 
 export default interface IUserService {
+  /*
+   * Since we return the user everytime it performs an API call
+   * we also want to return undefined if no user is found.
+   */
   getUser(userId: string): Promise<UserInfo | undefined>;
   login(username: string, password: string): Promise<UserInfo | undefined>;
   register(username: string, password: string): Promise<UserInfo | undefined>;
+  changePassword(
+    userId: string,
+    password: string
+  ): Promise<UserInfo | undefined>;
 }

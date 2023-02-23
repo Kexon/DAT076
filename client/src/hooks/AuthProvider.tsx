0fe11b0bde748f0f7e7eb5d21fe262d444e27e6c
emilsvennesson <emilsvennesson@users.noreply.github.com>
2023-02-23 15:41:25 +0100

    bug fixes in AuthProvider

--- client/src/hooks/AuthProvider.tsx
+++ client/src/hooks/AuthProvider.tsx
@@ -1,10 +1,14 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 import { UserInfo } from '../model/User';
+import UserService from '../services/UserService';
 
 interface AuthContextProps {
   user?: UserInfo;
+  login: (username: string, password: string) => Promise<void>;
+  loading: boolean;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const AuthContext = createContext<AuthContextProps>(null!);
 
 interface Props {
@@ -15,8 +19,25 @@ export function AuthProvider({ children }: Props) {
   const [user, setUser] = useState<UserInfo | undefined>();
   const [loading, setLoading] = useState(true);
 
+  const login = async (username: string, password: string) => {
+    setLoading(true);
+    const currentUser = await UserService.login(username, password);
+    setUser(currentUser);
+    setLoading(false);
+  };
+
   // eslint-disable-next-line react/jsx-no-constructed-context-values
-  const value = { user, loading };
+  const value = { login, user, loading };
+
+  useEffect(() => {
+    const getUser = async () => {
+      setLoading(true);
+      const currentUser = await UserService.getUser();
+      setUser(currentUser);
+      setLoading(false);
+    };
+    getUser();
+  }, []);
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }

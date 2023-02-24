b3a4ab241ae3f6faa485bef3a22259bcbbf06b6d
Pouya Shirin <kexon@hotmail.se>
2023-02-24 13:44:12 +0100

    change userpageinfo -> userpagesettings in dashboard

--- client/src/pages/user/dashboard/UserPage.tsx
+++ client/src/pages/user/dashboard/UserPage.tsx
@@ -1,6 +1,5 @@
 import { Button, Card, Label, TextInput } from 'flowbite-react';
-import { useState } from 'react';
-import UserPageSettings from './UserPageSettings';
+import UserPageInfo from './UserPageInfo';
 import UserTickets from './UserTickets';
 
 export default function UserPage() {
@@ -10,7 +9,10 @@ export default function UserPage() {
         <div className="flex flex-col gap-10">
           <h1 className="text-3xl font-semibold">Account panel</h1>
           <div className="flex flex-col gap-20 sm:flex-row">
-            <UserPageSettings />
+            <UserPageInfo />
+            <div>
+              <hr className="h-full w-px bg-gray-200 dark:bg-gray-700" />
+            </div>
             <UserTickets />
           </div>
         </div>
---
+++ client/src/pages/user/dashboard/UserPageInfo.tsx
@@ -0,0 +1,7 @@
+export default function UserPageInfo() {
+  return (
+    <div className="flex h-full w-full flex-col justify-start gap-6">
+      <h2 className="text-lg font-medium">Account info</h2>
+    </div>
+  );
+}

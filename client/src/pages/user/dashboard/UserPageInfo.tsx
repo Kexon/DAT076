a148893d4011a25c80378574113b650824f18eb0
Pouya Shirin <kexon@hotmail.se>
2023-02-24 13:46:05 +0100

    add placeholder text

--- client/src/pages/user/dashboard/UserPageInfo.tsx
+++ client/src/pages/user/dashboard/UserPageInfo.tsx
@@ -2,6 +2,11 @@ export default function UserPageInfo() {
   return (
     <div className="flex h-full w-full flex-col justify-start gap-6">
       <h2 className="text-lg font-medium">Account info</h2>
+      <p className="">
+        Skavlan var en svensk-norsk pratshow i SVT och TV2 Norge ledd av den
+        norske programledaren Fredrik Skavlan. Programmet hade premiär i Sverige
+        16 januari 2009 och 17 januari 2009 i Norge.
+      </p>
     </div>
   );
 }

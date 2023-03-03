bc6e6d6d615e337735f453b467f96ca8eca55996
Albin Sundström <albinsundstrom@hotmail.com>
2023-03-03 16:54:53 +0100

    add hamburger menu

---
+++ client/src/components/Hamburger.tsx
@@ -0,0 +1,58 @@
+import { ListGroup } from 'flowbite-react';
+import { useState } from 'react';
+import { HiMenu, HiX } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../hooks/AuthProvider';
+import { UserLevel } from '../model/User';
+
+export default function Hamburger() {
+  const [isOpen, setIsOpen] = useState(false);
+  const { logout, user } = useAuth();
+  const isAdmin = user && user.level >= UserLevel.ADMIN;
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleSignOut = () => {
+    logout();
+  };
+
+  return (
+    <div className="mx-2 mb-2 md:hidden">
+      <button
+        type="button"
+        className="rounded-md p-2 text-blue-500"
+        onClick={toggleMenu}
+      >
+        {isOpen ? <HiX className="h-6 w-6" /> : <HiMenu className="h-6 w-6" />}
+      </button>
+      {isOpen && (
+        <ListGroup className="flex-row items-center">
+          <Link to="/user/tickets">
+            <ListGroup.Item>My tickets</ListGroup.Item>
+          </Link>
+          <Link to="/user/createticket">
+            <ListGroup.Item>Create ticket</ListGroup.Item>
+          </Link>
+          {isAdmin && (
+            <div>
+              <hr className="mx-2" />
+              <Link to="/user/alltickets">
+                <ListGroup.Item>All tickets</ListGroup.Item>
+              </Link>
+              <Link to="/user/users">
+                <ListGroup.Item>Users</ListGroup.Item>
+              </Link>
+            </div>
+          )}
+          <hr className="mx-2" />
+          <Link to="/user/settings">
+            <ListGroup.Item>Settings</ListGroup.Item>
+          </Link>
+          <ListGroup.Item onClick={handleSignOut}>Sign out</ListGroup.Item>
+        </ListGroup>
+      )}
+    </div>
+  );
+}

ccb272eb1e9c88cd5baaf93705cb155b6dee9dad
Albin Sundström <albinsundstrom@hotmail.com>
2023-03-10 11:50:41 +0100

    setup db testing

--- server/src/db/conn.ts
+++ server/src/db/conn.ts
@@ -1,8 +1,19 @@
-import "dotenv/config";
-import { createConnection } from "mongoose";
+import 'dotenv/config';
+import { createConnection } from 'mongoose';
+
+let uri: string;
 
 if (!process.env.DB_URI) {
-  throw new Error("DB_URI not found");
+  throw new Error('DB_URI not found');
+}
+if (!process.env.DB_URI_TEST) {
+  throw new Error('DB_URI_TEST not found');
 }
 
-export const conn = createConnection(process.env.DB_URI);
+if (process.env.NODE_ENV === 'test') {
+  uri = process.env.DB_URI_TEST;
+} else {
+  uri = process.env.DB_URI;
+}
+
+export const conn = createConnection(uri);
